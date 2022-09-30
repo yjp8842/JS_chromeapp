defda4ba337c949ee63c0327190c807132284ee6
yjp8842 <yjp20183029@gmail.com>
2022-09-30 17:50:00 +0900

    js

--- JS_chromeapp/js/background.js
+++ JS_chromeapp/js/background.js
@@ -1,9 +1,10 @@
-const images = ["img1.jpg", "img2.jpg"];
+const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+document.getElementById("background").style.backgroundImage = `url('img/${chosenImage}')`;
