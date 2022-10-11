ce456553d8410683ca0c020de99262d7940d67ff
yjp8842 <yjp20183029@gmail.com>
2022-10-11 16:09:51 +0900

    js

--- JS_chromeapp/js/youtube.js
+++ JS_chromeapp/js/youtube.js
@@ -1,48 +1,27 @@
-// 2. 이 코드는 IFrame Player API 코드를 비동기 적으로로드합니다.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3.이 함수는 <iframe> (및 YouTube 플레이어)을 만듭니다.API 코드 다운로드 후.
-
 var player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height: '360',
 		width: '640',
-		videoId: "pVcMsjyKlaM",
+		videoId: "eR5DtxrwCms",
 		events: {}
 	});
 }
 
-// // 4. API는 동영상 플레이어가 준비되면이 함수를 호출합니다.
-// function onPlayerReady(event) {
-// 	event.target.playVideo();
-// }
-
-// // 5. API는 플레이어의 상태가 변경 될 때이 함수를 호출합니다.이 함수는 비디오 
-// //(상태 = 1)를 재생할 때 플레이어는 0 초 동안 재생 한 다음 중지해야합니다.
-// var done = false;
-// function onPlayerStateChange(event) {
-// 	if (event.data == YT.PlayerState.PLAYING && !done) {
-// 			setTimeout(stopVideo, 0000);
-// 			done = true;
-// 	}
-// }
-
-// function stopVideo() {
-//   player.stopVideo();
-// }
 const PLAYLIST_ID = [
   {
-    name: "Miles Teller - Great Balls of Fire",
-    videoId: "pVcMsjyKlaM",
+    name: "10cm - 서랍",
+    videoId: "eR5DtxrwCms",
   },
   {
-    name: 'OneRepublic - I Aint Worried',
-    videoID: '42oK5vjD2UU',
+    name: '샘킴 - 여름비',
+    videoId: 'Ga5jVpIfrBY',
   },
 ];
 
@@ -50,7 +29,7 @@ const playList = document.querySelector("#playList");
 
 function handlePlayBtnClick() {
   player.playVideo();
-  playList.innerText = `List ${currentPlayerList + 1} - ${PLAYLIST_ID[currentPlayerList].name}`;
+  playList.innerText = `PLAY ${currentPlayerList + 1} - ${PLAYLIST_ID[currentPlayerList].name}`;
 }
 
 function handlePauseBtnClick() {
@@ -65,7 +44,7 @@ function handleNextBtnClick() {
     currentPlayerList += 1;
   }
   player.loadVideoById(PLAYLIST_ID[currentPlayerList].videoId);
-  playList.innerText = `List ${currentPlayerList + 1} - ${PLAYLIST_ID[currentPlayerList].name}`;
+  playList.innerText = `PLAY ${currentPlayerList + 1} - ${PLAYLIST_ID[currentPlayerList].name}`;
 }
 
 const playButton = document.querySelector("#play");
